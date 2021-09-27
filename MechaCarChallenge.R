1857df1c9682c291b605e434bbe55b9bdbda09a4
Mbehr <morganbehr11@gmail.com>
2021-09-26 23:25:37 -0600

    adding all pgn files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,8 +7,7 @@ summary(mecha_lm)
 
 suspension_coil_df<-read.csv(file='Suspension_Coil.csv',check.names = F,stringsAsFactors =F)
 
-total_summary <- suspension_coil_df %>%
-  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+total_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
 
